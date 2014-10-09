ddca89cf090f688314df0159c4d7ce878de9973e
jdgwartney <davidg@boundary.com>
2014-10-08 17:36:20 -0700

    Updates

--- src/test/scripts/start-cluster.sh
+++ src/test/scripts/start-cluster.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+set -x
+
+
 StartNodes() {
    env RABBITMQ_NODE_PORT=5672 RABBITMQ_NODENAME=rabbit   rabbitmq-server -detached
    env RABBITMQ_NODE_PORT=5673 RABBITMQ_NODENAME=rabbit_1 rabbitmq-server -detached
    env RABBITMQ_NODE_PORT=5674 RABBITMQ_NODENAME=rabbit_2 rabbitmq-server -detached
+}
+
+
+AddRabbit1ToCluster() {
+   rabbitmqctl -n rabbit_1@lerma cluster rabbit@lerma rabbit_1@lerma
 }
