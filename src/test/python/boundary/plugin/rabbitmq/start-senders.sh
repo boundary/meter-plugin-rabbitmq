bda0d0612fbe4ae01701d71caf7161bd77253c07
jdgwartney <davidg@boundary.com>
2014-10-08 17:38:14 -0700

    test script

---
+++ src/test/python/boundary/plugin/rabbitmq/start-senders.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CMD="while : ; do ./send.py ; sleep 10 ;  done"
+
+bash -c "$CMD" &
+bash -c "$CMD" &
+bash -c "$CMD" &
+bash -c "$CMD" &
+bash -c "$CMD" &
+bash -c "$CMD" &
+bash -c "$CMD" &
+bash -c "$CMD" &
+bash -c "$CMD" &
+bash -c "$CMD" &
